941c4b3909a1fd2120155006bc6524e03e1b7262
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-12-12 15:48:26 +0800

    impl infrastructure layer(tables, impl repo, mapper)

---
+++ src/infrastructure/mappers/borrow_mapper.py
@@ -0,0 +1,27 @@
+from src.features.borrow.domain.entity import BorrowEntity
+from src.features.borrow.domain.value_objects import DateOfBorrow, DateOfReturn
+from src.infrastructure.models import BorrowModel
+
+
+class BorrowMapper:
+    @staticmethod
+    def entity_to_model(entity: BorrowEntity) -> BorrowModel:
+        return BorrowModel(
+            book_id=entity.book_id,
+            reader_name=entity.reader_name,
+            date_of_borrow=entity.date_of_borrow.value,
+            date_of_return=entity.date_of_return.value if entity.date_of_return is not None else None,
+        )
+
+    @staticmethod
+    def model_to_entity(model: BorrowModel) -> tuple[BorrowEntity, str]:
+        return (
+            BorrowEntity(
+                book_id=model.book_id,
+                reader_name=model.reader_name,
+                date_of_borrow=DateOfBorrow(model.date_of_borrow),
+                date_of_return=DateOfReturn(model.date_of_return) if model.date_of_return  else None,
+                borrow_id=model.id,
+            ),
+            model.book.title,
+        )
---
+++ src/infrastructure/repositories/__init__.py
@@ -0,0 +1,10 @@
+from .author_repository import AuthorRepository
+from .book_repository import BookRepository
+from .borrow_repository import BorrowRepository
+
+
+__all__ = (
+    'AuthorRepository',
+    'BookRepository',
+    'BorrowRepository',
+)
